0ee9c40b54772f06dae33a664b68638898694e52
hallaoppah2 <enochoppah2@gmail.com>
2024-04-29 19:44:57 +0000

    first commit

---
+++ demo.java
@@ -0,0 +1,30 @@
+public class demo {
+
+    public static void main(String[] args){
+
+        //display
+
+        System.out.println("My nane is Oppah, please be guided");
+        System.out.println("I want to be the best");
+        System.out.println("Java is interesting");
+        System.out.println(5);
+        System.out.println(5 + 8);
+        System.out.println("This is multiplication: " + 5 * 8);
+
+        //variable
+
+        String name = "Oppah";
+        String lname = "Enoch";
+        System.out.println(name + " " + lname); 
+        System.out.println("Hello " + name + ", how are you doing? " + name); 
+
+        //casting
+
+        int age = 12;
+        double age1 = age;
+
+        System.out.println(age);
+        System.out.println(age1);
+
+    }
+}
